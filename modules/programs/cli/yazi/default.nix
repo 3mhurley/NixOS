41a98f918d6dfacda3fc554a6610c418e0895cdf
Evan Hurley <3mhurley@gmail.com>
2026-02-15 11:24:48 -0700

    Adjust build process for firefox

--- modules/programs/cli/yazi/default.nix
+++ modules/programs/cli/yazi/default.nix
@@ -6,6 +6,7 @@
         enable = true;
         enableBashIntegration = true;
         enableZshIntegration = true;
+        shellWrapperName = "yy";
         settings = {
           mgr = {
             show_hidden = true;
